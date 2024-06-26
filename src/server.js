5b3c691b6a007669395c0883678eebdd76259a60
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-06-26 21:32:18 +0200

    new paths added

--- src/server.js
+++ src/server.js
@@ -3,16 +3,13 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
 dotenv.config();
 const PORT = Number(env('PORT'));
 
 export const startServer = () => {
   const app = express();
-
-  app.use(express.json());
-  app.use(cors());
-
   app.use(
     pino({
       transport: {
@@ -21,9 +18,38 @@ export const startServer = () => {
     }),
   );
 
+  app.use(express.json());
+  app.use(cors());
+
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello world!',
+    });
+  });
+
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+
+    res.status(200).json({
+      data: students,
+    });
+  });
+
+  app.get('/students/:studentId', async (req, res, next) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      data: student,
     });
   });
 
