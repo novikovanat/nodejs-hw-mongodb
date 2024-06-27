a5fa90468f7dbaf41eb6e561760097d95919875e
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-06-27 22:18:45 +0200

    status added

--- src/server.js
+++ src/server.js
@@ -21,45 +21,58 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  app.get('/', (req, res) =>
-    res.json({
-      message: 'Hello, World!',
-    }),
-  );
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status:200,
       data: contacts,
+      message: "Successfully found contacts!"
     });
   });
 
   app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+    try {
+      const contact = await getContactById(contactId);
 
-    // Відповідь, якщо контакт не знайдено
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
+      // Відповідь, якщо контакт не знайдено
+      if (!contact) {
+        res.status(404).json({
+          status: 404,
+          message: 'Contact not found',
+        });
+        return;
+      }
+
+      // Відповідь, якщо контакт знайдено
+      res.status(200).json({
+        status:200,
+        data: contact,
+        message: `Successfully found contact with id ${contactId}!`
       });
-      return;
+    } catch (error) {
+      if (error.message.includes('Cast to ObjectId')) {
+        error.status = 404;
+      }
+      const { status = 500 } = error;
+      res.status(status).json({
+        status: status,
+        message: error.message,
+      });
     }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      data: contact,
-    });
   });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
-      message: 'Not found',
+      status: 404,
+      message: 'Page not found',
     });
   });
 
   app.use((err, req, res, next) => {
     res.status(500).json({
+      status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
