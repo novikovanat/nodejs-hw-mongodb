4a5a868d0408e156f873efc6151ea62b2f380236
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-06-27 15:35:17 +0200

    contacts

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,4 +1,4 @@
-import { model, Schema } from 'mongoose';
+import {model, Schema } from 'mongoose';
 import { emailRegex } from '../../constants/contact-constants.js';
 
 function validator(email) {
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/students.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 dotenv.config();
 const PORT = Number(env('PORT'));
@@ -21,30 +21,34 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
 
-
-  app.get('/students', async (req, res) => {
-    const students = await getAllContacts();
+  app.get('/', (req, res) =>
+    res.json({
+      message: 'Hello, World!',
+    }),
+  );
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
 
     res.status(200).json({
-      data: students,
+      data: contacts,
     });
   });
 
   app.get('/students/:studentId', async (req, res, next) => {
-    const { studentId } = req.params;
-    const student = await getContactById(studentId);
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
 
     // Відповідь, якщо контакт не знайдено
-    if (!student) {
+    if (!contact) {
       res.status(404).json({
-        message: 'Student not found',
+        message: 'Contact not found',
       });
       return;
     }
 
     // Відповідь, якщо контакт знайдено
     res.status(200).json({
-      data: student,
+      data: contact,
     });
   });
 
