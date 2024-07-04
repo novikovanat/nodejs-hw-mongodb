88fa4bc8aa85df948ac97873c72d9b11e4e56c23
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-04 09:52:02 +0200

    patch route added

--- src/controllers/studentsControllers.js
+++ src/controllers/studentsControllers.js
@@ -73,3 +73,19 @@ res.status(status).json({
   data: result.student,
 });
 };
+
+export const patchStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await updateStudent(studentId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a student!`,
+    data: result.student,
+  });
+};
--- src/routers/studentsRouters.js
+++ src/routers/studentsRouters.js
@@ -6,6 +6,7 @@ import {
   createStudentController,
   deleteStudentController,
   upsertStudentController,
+  patchStudentController,
 } from '../controllers/studentsControllers.js';
 
 import controllerWrapper from '../utils/controllerWrapper.js';
@@ -17,5 +18,5 @@ router.get('/:studentId', controllerWrapper(getStudentByIdController));
 router.post('/', controllerWrapper(createStudentController));
 router.delete('/:studentId', controllerWrapper(deleteStudentController));
 router.put('/:studentId', controllerWrapper(upsertStudentController));
-
+router.patch('/:studentId', controllerWrapper(patchStudentController));
 export default router;
