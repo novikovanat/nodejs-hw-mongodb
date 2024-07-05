e0a5f008bfaa2b1a717b9526998f9c3b40222190
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-05 22:15:26 +0200

    route patch added

--- src/controllers/contactsControllers.js
+++ src/controllers/contactsControllers.js
@@ -1,40 +1,64 @@
-import { createContact, getAllContacts, getContactById } from '../services/contactsServices.js';
+import {
+  createContact,
+  getAllContacts,
+  getContactById,
+  updateContact,
+} from '../services/contactsServices.js';
 import createHttpError from 'http-errors';
 
-export const getAllContactsController= async (req, res) => {
-    const contacts = await getAllContacts();
+export const getAllContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
 
-    res.status(200).json({
-      status:200,
-      data: contacts,
-      message: "Successfully found contacts!"
-    });
-  };
+  res.status(200).json({
+    status: 200,
+    data: contacts,
+    message: 'Successfully found contacts!',
+  });
+};
 
-  export const getContactByIdController = async (req, res, next) => {
-    const { contactId } = req.params;
-      const contact = await getContactById(contactId);
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
 
-      // Відповідь, якщо контакт не знайдено
-      if (!contact) {
-       next(createHttpError(404, 'Contact not found'));
-        return;
-      }
+  // Відповідь, якщо контакт не знайдено
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
 
-      // Відповідь, якщо контакт знайдено
-      res.status(200).json({
-        status:200,
-        data: contact,
-        message: `Successfully found contact with id ${contactId}!`
-      });
-  };
+  // Відповідь, якщо контакт знайдено
+  res.status(200).json({
+    status: 200,
+    data: contact,
+    message: `Successfully found contact with id ${contactId}!`,
+  });
+};
 
-  export const createContactController = async (req, res) => {
-    const contact = createContact(req.body);
+export const createContactController = async (req, res) => {
+  const contact = createContact(req.body);
 
-    res.status(201).json({
-      status: 201,
-      message: "Successfully created a contact!",
-      data: contact,
-    });
-  };
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: contact,
+  });
+};
+
+export const updateContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: "Successfully patched a contact!",
+    data: result.student,
+  });
+};
--- src/routers/contactsRouters.js
+++ src/routers/contactsRouters.js
@@ -4,6 +4,7 @@ import {
   createContactController,
   getAllContactsController,
   getContactByIdController,
+  updateContactController,
 } from '../controllers/contactsControllers.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -13,3 +14,5 @@ router.get('/', ctrlWrapper(getAllContactsController));
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
 
 router.post('/', ctrlWrapper(createContactController));
+
+router.patch('/:contactId', ctrlWrapper(updateContactController));
