459d31cf4a32e2115a68bf8b9caef6d739d3e0ba
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-05 09:05:43 +0200

    routes separated

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contactsServices.js';
+import { router } from './routers/contactsRouters.js';
 
 dotenv.config();
 const PORT = Number(env('PORT'));
@@ -20,48 +20,8 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use('/contacts', router);
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status:200,
-      data: contacts,
-      message: "Successfully found contacts!"
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    try {
-      const contact = await getContactById(contactId);
-
-      // Відповідь, якщо контакт не знайдено
-      if (!contact) {
-        res.status(404).json({
-          status: 404,
-          message: 'Contact not found',
-        });
-        return;
-      }
-
-      // Відповідь, якщо контакт знайдено
-      res.status(200).json({
-        status:200,
-        data: contact,
-        message: `Successfully found contact with id ${contactId}!`
-      });
-    } catch (error) {
-      if (error.message.includes('Cast to ObjectId')) {
-        error.status = 404;
-      }
-      const { status = 500 } = error;
-      res.status(status).json({
-        status: status,
-        message: error.message,
-      });
-    }
-  });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
