cae043cf7b1ab22d31013434c2ad4e2be671f650
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-05 09:12:54 +0200

    controllers separated

--- src/routers/contactsRouters.js
+++ src/routers/contactsRouters.js
@@ -1,46 +1,11 @@
-import { Router } from "express";
-import { getAllContacts, getContactById } from '../services/contactsServices.js';
+import { Router } from 'express';
 
+import {
+  getAllContactsController,
+  getContactByIdController,
+} from '../controllers/contactsControllers.js';
 
 export const router = Router();
-router.get('/', async (req, res) => {
-    const contacts = await getAllContacts();
+router.get('/', getAllContactsController);
 
-    res.status(200).json({
-      status:200,
-      data: contacts,
-      message: "Successfully found contacts!"
-    });
-  });
-
-router.get('/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    try {
-      const contact = await getContactById(contactId);
-
-      // Відповідь, якщо контакт не знайдено
-      if (!contact) {
-        res.status(404).json({
-          status: 404,
-          message: 'Contact not found',
-        });
-        return;
-      }
-
-      // Відповідь, якщо контакт знайдено
-      res.status(200).json({
-        status:200,
-        data: contact,
-        message: `Successfully found contact with id ${contactId}!`
-      });
-    } catch (error) {
-      if (error.message.includes('Cast to ObjectId')) {
-        error.status = 404;
-      }
-      const { status = 500 } = error;
-      res.status(status).json({
-        status: status,
-        message: error.message,
-      });
-    }
-  });
+router.get('/:contactId', getContactByIdController);
