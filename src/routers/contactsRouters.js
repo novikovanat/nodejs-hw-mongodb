aedb65c7d9cc3a83912cd36ff177823d673fdd84
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-13 15:01:03 +0200

    joi validation is added

--- src/routers/contactsRouters.js
+++ src/routers/contactsRouters.js
@@ -10,7 +10,7 @@ import {
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { isValidId } from '../middleware/IsValidId.js';
 import { validateBody } from '../middleware/validateBody.js';
-import { createContactSchema } from '../validation/contactsValidation.js';
+import { createContactSchema, updateContactSchema } from '../validation/contactsValidation.js';
 
 
 export const router = Router();
@@ -20,6 +20,6 @@ router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
 router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-router.patch('/:contactId',isValidId, ctrlWrapper(updateContactController));
+router.patch('/:contactId',isValidId, validateBody(updateContactSchema), ctrlWrapper(updateContactController));
 
 router.delete('/:contactId',ctrlWrapper(deleteContactController) );
--- src/validation/contactsValidation.js
+++ src/validation/contactsValidation.js
@@ -1,21 +1,26 @@
 import Joi from 'joi';
-import { CONTACTS_TYPE, DEFAULT_CONTACTS_TYPE, phoneNumberRegex } from '../constants/contact-constants.js';
-
+import {
+  CONTACTS_TYPE,
+  DEFAULT_CONTACTS_TYPE,
+  phoneNumberRegex,
+} from '../constants/contact-constants.js';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
+  phoneNumber: Joi.string().pattern(phoneNumberRegex).required(),
+  email: Joi.string().email().allow(null).default(null),
+  contactType: Joi.string()
+    .valid(...CONTACTS_TYPE)
+    .default(DEFAULT_CONTACTS_TYPE),
+  isFavourite: Joi.boolean().default(false),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
   phoneNumber: Joi.string().pattern(phoneNumberRegex),
   email: Joi.string().email().allow(null).default(null),
-  contactType: Joi.string().valid(...CONTACTS_TYPE).default(DEFAULT_CONTACTS_TYPE),
-  isFavourite: Joi.boolean().default(false)
+  contactType: Joi.string()
+    .valid(...CONTACTS_TYPE)
+    .default(DEFAULT_CONTACTS_TYPE),
+  isFavourite: Joi.boolean().default(false),
 });
-
-export const updateStudentSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
-  // email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
-});
-
