7e354b87e04d481c54b6278e398f9ef46ec0950f
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-13 10:05:28 +0200

    id check added

--- src/routers/contactsRouters.js
+++ src/routers/contactsRouters.js
@@ -3,19 +3,21 @@ import { Router } from 'express';
 import {
   createContactController,
   deleteContactController,
-  getAllContactsController,
+  getContactsController,
   getContactByIdController,
   updateContactController,
 } from '../controllers/contactsControllers.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middleware/IsValidId.js';
+
 
 export const router = Router();
-router.get('/', ctrlWrapper(getAllContactsController));
+router.get('/', ctrlWrapper(getContactsController));
 
-router.get('/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
 router.post('/', ctrlWrapper(createContactController));
 
-router.patch('/:contactId', ctrlWrapper(updateContactController));
+router.patch('/:contactId',isValidId, ctrlWrapper(updateContactController));
 
 router.delete('/:contactId',ctrlWrapper(deleteContactController) );
