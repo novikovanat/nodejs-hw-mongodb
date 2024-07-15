5964499be4770345d3f41ad32ee70790c30ac84e
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-15 22:18:37 +0200

    authentication  added

--- src/constants/studentConstants.js
+++ src/constants/studentConstants.js
@@ -15,3 +15,7 @@ export const emailRegex =
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const ROLES = {
+  TEACHER: 'teacher',
+  PARENT: 'parent',
+};
--- src/routers/studentsRouters.js
+++ src/routers/studentsRouters.js
@@ -17,30 +17,44 @@ import {
 } from '../validation/studentsValidation.js';
 import { isValidId } from '../middleware/IsValidId.js';
 import { authenticate } from '../services/authenticate.js';
+import { checkRoles } from '../middleware/checkRoles.js';
+import { ROLES } from '../constants/studentConstants.js';
 
 const StudentsRouter = Router();
 
 StudentsRouter.use(authenticate);
-StudentsRouter.get('/', controllerWrapper(getStudentsController));
+StudentsRouter.get(
+  '/',
+  checkRoles(ROLES.TEACHER),
+  controllerWrapper(getStudentsController),
+);
 StudentsRouter.get(
   '/:studentId',
-   isValidId,
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  isValidId,
   controllerWrapper(getStudentByIdController),
 );
 StudentsRouter.post(
   '/',
   validateBody(createStudentSchema),
+  checkRoles(ROLES.TEACHER),
   controllerWrapper(createStudentController),
 );
-StudentsRouter.delete('/:studentId', controllerWrapper(deleteStudentController));
+StudentsRouter.delete(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  controllerWrapper(deleteStudentController),
+);
 StudentsRouter.put(
   '/:studentId',
   validateBody(updateStudentSchema),
   isValidId,
+  checkRoles(ROLES.TEACHER),
   controllerWrapper(upsertStudentController),
 );
 StudentsRouter.patch(
   '/:studentId',
+  checkRoles(ROLES.TEACHER),
   validateBody(updateStudentSchema),
   isValidId,
   controllerWrapper(patchStudentController),
