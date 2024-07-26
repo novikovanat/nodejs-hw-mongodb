408d7741d5156913086635c3e449957b8ca4175b
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-26 16:43:14 +0200

    const added

--- src/constants/contactConstants.js
+++ src/constants/contactConstants.js
@@ -16,6 +16,7 @@ export const SORT_ORDER_ARRAY = ['asc', 'desc'];
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 export const ROLES = {
   TEACHER: 'teacher',
   PARENT: 'parent',
