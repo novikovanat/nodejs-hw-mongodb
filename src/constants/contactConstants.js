5be4880042047ecb51fb51f485400d8acd8eaba2
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-07-28 16:20:52 +0200

    get contacts description

--- src/constants/contactConstants.js
+++ src/constants/contactConstants.js
@@ -21,10 +21,6 @@ export const KEYS_OF_COLLECTION = [
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
-export const ROLES = {
-  TEACHER: 'teacher',
-  PARENT: 'parent',
-};
 
 export const SMTP = {
   SMTP_HOST: 'SMTP_HOST',
